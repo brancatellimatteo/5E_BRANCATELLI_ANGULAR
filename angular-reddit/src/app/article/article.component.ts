18b234f57b91f6c7b1228b60842860797f9fa6a5
EC2 Default User <ec2-user@ip-172-31-93-45.ec2.internal>
2019-04-03 17:38:53 +0000

    primo commit

---
+++ angular-reddit/src/app/article/article.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'card';
+  votes: number;
+  title: string;
+  link: string;
+  
+  constructor() { 
+    this.link = 'http://angular.io';
+    this.title = 'Angular 2';
+    this.votes = 10;
+  }
+  
+  voteUp():Boolean{
+    this.votes += 1;
+    return false;
+  }
+  
+  voteDown():Boolean{
+    this.votes -=1;
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}

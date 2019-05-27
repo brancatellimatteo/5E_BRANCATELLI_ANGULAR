573041ee22826096799422a49b2bfdcfd7a8ed8c
brancatellimatteo <brancatelli.matteo@iisgalvanimi.edu.it>
2019-05-27 19:31:02 +0000

    parzialmente completo d

--- eserciziov2/src/app/app.module.ts
+++ eserciziov2/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { DrivingListComponent } from './driving-list/driving-list.component';
+import { DrivingDetailComponent } from './driving-detail/driving-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DrivingListComponent,
+    DrivingDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, ReactiveFormsModule, NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
